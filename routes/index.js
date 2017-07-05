606d626c317c00b14c1a7cf6e18972affe8eb190
LANWORKS\thang.nguyen <quangthang99@gmail.com>
2017-07-05 17:59:24 +0700

    no message

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,17 @@ module.exports = function(io){
 	var app = require('express');
 	var router = app.Router();
 	var fs = require('fs');
+	var drawing = new Array();
 
 	router.get('/', function(req, res, next){
-		res.render('index', {title: 'DEMO SOCKET.IO'});
+		// loadAllDrawings();
+		res.render('index', {title: 'DEMO SOCKET.IO', drawing: drawing});
 	});
 
 	io.of('/').on('connection', function(socket){
 		socket.on('drawing', function(data){
 			// receive data and write to a file
-			storeDrawing(data);
+			// storeDrawing(data);
 			socket.broadcast.emit('drawing', data);
 		});
 	});
@@ -21,13 +23,16 @@ module.exports = function(io){
 				throw err;
 
 			// write data to file
-
+			fs.write(fd, data.x1, 0, 8, null, null);
+			fs.write(fd, data.y1, 0, 8, null, null);
+			fs.write(fd, data.x2, 0, 8, null, null);
+			fs.write(fd, data.y2, 0, 8, null, null);
 			// close file when done
 			fs.close(fd, null);
 		});
 	}
 
-	function loadAllDrawing(){
+	function loadAllDrawings(){
 		fs.open('../drawing.bin', 'r', (err, fd) => {
 			if(err)
 				throw err;
